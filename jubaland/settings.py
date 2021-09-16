7c9109aecef21cb8a8cb97c8bcef46e9f4fd492c
Abdisamad100 <abdisamadcade004@gmail.com>
2021-09-16 06:03:18 +0300

    deployment

--- jubaland/settings.py
+++ jubaland/settings.py
@@ -32,8 +32,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
 
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 # Application definition
 
